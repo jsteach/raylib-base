dd786e55f63e933f30639a2a8c1c443c5bf1cbf3
00547 <00547@bart.ca>
2025-03-13 21:03:39 -0400

    Add automatique zig download when building

--- setup.bat
+++ setup.bat
@@ -1,3 +1,6 @@
-.\Tools\wget.com https://ziglang.org/download/0.11.0/zig-windows-x86_64-0.11.0.zip
-.\Tools\unzip.com -j zig-windows-x86_64-0.11.0.zip zig-windows-x86_64-0.11.0/zig.exe -d ./Tools/windows_x64
-del .\zig-windows-x86_64-0.11.0.zip
+@echo off
+IF NOT EXIST "Tools\windows_x64\zig.exe" (
+    .\Tools\wget.com https://ziglang.org/download/0.11.0/zig-windows-x86_64-0.11.0.zip
+    .\Tools\unzip.com -j zig-windows-x86_64-0.11.0.zip zig-windows-x86_64-0.11.0/zig.exe -d ./Tools/windows_x64
+    del .\zig-windows-x86_64-0.11.0.zip
+)
